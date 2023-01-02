6ea8734749a0d1388b19ecc2dea702d937aa759a
Bamboozle-jpg <kevin@bachelorclan.com>
2023-01-02 13:05:37 -0800

    host localhost doesn't work, need to use 127.0.0.1

--- phpMySQLConnect.php
+++ phpMySQLConnect.php
@@ -1,10 +1,15 @@
 <?php
-    require_once 'dbconfig.php';
+// include 'dbconfig.php';
+$host = '127.0.0.1';
+$dbname = 'myTestDatabase';
+$username = 'root';
+$password = '';
 
-    try {
-        $conn = new PDO ("mysql:host=$host;dbname=$dbname", $username, $password);
-        echo "I'm in :sunglasses: (to $dbname at $host)";
-    } catch (PDOException $pe) {
-        die("Couldn't connect to database : $dbname :" . $pe->getMessage());
-    }
->
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    echo "Connected to $dbname at $host successfully.";
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+}
+    exit;
+?>
