4e6ac14fe5adf0a8cfe4f3db2d871e18d4b77232
Bamboozle-jpg <kevin@bachelorclan.com>
2023-01-02 12:31:55 -0800

    Moved folders around

--- phpMySQLConnect.php
+++ phpMySQLConnect.php
@@ -2,5 +2,9 @@
     require_once 'dbconfig.php';
 
     try {
-        $conn = new PDO ("mysql:host=$host;dbname=$dbname", $username, $password)
+        $conn = new PDO ("mysql:host=$host;dbname=$dbname", $username, $password);
+        echo "I'm in :sunglasses: (to $dbname at $host)";
+    } catch (PDOException $pe) {
+        die("Couldn't connect to database : $dbname :" . $pe->getMessage());
     }
+>
